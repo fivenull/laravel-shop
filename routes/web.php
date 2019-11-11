1fe793d148b78872f735479aa786137e0892fef3
fivenull <fivenull@163.com>
2019-11-11 02:58:43 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Auth::routes(['verify' => true]);
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
@@ -25,4 +25,6 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
